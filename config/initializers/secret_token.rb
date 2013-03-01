7436b2548263910f3f85c0c11ed950e403164702
Alejandro Guerrero <agg.alex@gmail.com>
2013-03-01 14:14:00 -0600

    New vanilla ruby app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenRap::Application.config.secret_token = '347ca287550e358dcef0872802be4ebc55d63134c35887b60014a85a94ceb111ad4f14f4a6fabc6ffcebd3a4e0a2d439c86627b081c0294bca02f2392b445c21'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GenRap::Application.config.session_store :cookie_store, :key => '_GenRap_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GenRap::Application.config.session_store :active_record_store
