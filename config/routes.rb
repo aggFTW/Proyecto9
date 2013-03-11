6a3c8ffe70d72cd04f96a071518d65c45948014a
Alejandro Guerrero <alexgg@Alejandros-MacBook-Pro-3.local>
2013-03-11 04:32:53 -0600

    login and sessions implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 GenRap::Application.routes.draw do
+  # get "sessions/login,"
+
+  # get "sessions/home,"
+
+  # get "sessions/profile,"
+
+  # get "sessions/setting"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   root :to => 'users#new'
 
   resources :users
+  
+  match "signup", :to => "users#new"
+  match "login", :to => "sessions#login"
+  match "logout", :to => "sessions#logout"
+  # match "home", :to => "sessions#home"
+  # match "profile", :to => "sessions#profile"
+  # match "setting", :to => "sessions#setting"
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
